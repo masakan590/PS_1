2b55aed79086ed75e88c9e52cae4059fdc2866f2
masakan590 <113155308+masakan590@users.noreply.github.com>
2023-11-28 14:50:05 +0900

    プレイヤーの生成が完了

--- PS_1/PS_1/10/main_scene.js
+++ PS_1/PS_1/10/main_scene.js
@@ -10,7 +10,16 @@ class MainScene extends Phaser.Scene {
         super('MainScene');
     }
 
-    
+    preload()
+    {
+        this.load.image('taro', 'assets/taro.png');
+        this.load.image('background', 'assets/background.png');
+    }
+
+    create()
+    {
+        this.add.image(400, 300, 'taro');
+    }
 
 }
 
