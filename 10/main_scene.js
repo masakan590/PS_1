38515fc89022ec476c22f0c6d2e82d45b01e2858
masakan590 <113155308+masakan590@users.noreply.github.com>
2023-12-04 13:56:21 +0900

    1-5の完成

--- 10/main_scene.js
+++ 10/main_scene.js
@@ -13,7 +13,7 @@ class MainScene extends Phaser.Scene {
     preload()
     {
         this.load.image('taro', 'assets/taro.png');
-        this.load.image('jori', 'assets/jiro.png');
+        this.load.image('jiro', 'assets/jiro.png');
         this.load.image('background', 'assets/background.png');
     }
 
@@ -24,21 +24,41 @@ class MainScene extends Phaser.Scene {
         this.taro.angle = 0
 
         this.jiro = this.physics.add.image(300, 200, 'jiro');
-        this,jiro.angle = 0
+        this.jiro.angle = 0
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update(time, delta) {
-        // this.taro.setVelocityX(20);
-        // this.taro.setVelocityY(20);
-        this.taro.angle += 5; // 5度ずつ角度を増やす
+        // // this.taro.setVelocityX(20);
+        // // this.taro.setVelocityY(20);
+        // this.taro.angle += 5; // 5度ずつ角度を増やす
 
-        // 45度方向に移動する
-        this.taro.setVelocityX(50); // +X方向に5の速度を設定
-        this.taro.setVelocityY(-50); // -Y方向に5の速度を設定
+        // // 45度方向に移動する
+        // this.taro.setVelocityX(50); // +X方向に5の速度を設定
+        // this.taro.setVelocityY(-50); // -Y方向に5の速度を設定
 
-        if (this.taro.x > this.sys.game.config.width || this.taro.y > this.sys.game.config.height) {
-            this.taro.x = this.sys.game.config.width / 2;
-            this.taro.y = this.sys.game.config.height / 2;
+        // if (this.taro.x > this.sys.game.config.width || this.taro.y > this.sys.game.config.height) {
+        //     this.taro.x = this.sys.game.config.width / 2;
+        //     this.taro.y = this.sys.game.config.height / 2;
+        //}
+
+         // taroの移動
+        if (this.cursors.right.isDown) {
+            this.taro.setVelocityX(50); // 右に50動く
+        } else if (this.cursors.left.isDown) {
+            this.taro.setVelocityX(-50); // 左に50動く
+        } else {
+            this.taro.setVelocityX(0); // キーが離されたら停止する
+        }
+
+        // jiroの移動
+        if (this.cursors.right.isDown) {
+            this.jiro.setVelocityX(-50); // 左に50動く
+        } else if (this.cursors.left.isDown) {
+            this.jiro.setVelocityX(50); // 右に50動く
+        } else {
+            this.jiro.setVelocityX(0); // キーが離されたら停止する
         }
     }
 }
